237347f7a672bf4f427d072759076eb0abd643c4
Madison Evans <m.corbinevans@gmail.com>
2023-01-16 22:18:21 -0700

    submit button works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	const form = document.querySelector("#create-task-form");
+	const ul = document.querySelector("#tasks");
+
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+
+		let newVal = document.createElement("li");
+		newVal.innerText = document.getElementById("new-task-description").value;
+		ul.append(newVal);
+
+		e.target.reset();
+	});
 });
